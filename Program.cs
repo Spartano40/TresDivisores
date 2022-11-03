82536cebbe0efef69e077b839daf95316551a21c
Marlon Wesley de Paula <marlonestudanteadm31@hotmail.com>
2022-11-02 21:37:22 -0300

    TresDivisores

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+ 
+class Solution {
+    public static void Main(string[] args) {
+        int numeroTestado = int.Parse(Console.ReadLine());
+        int quantidadeDeDivisores = 0;
+ 
+        for (int i = 1; i <= numeroTestado; i++) {
+// TODO: Crie as outras condições necessárias para a resolução do desafio:
+            if (numeroTestado % i == 0) {
+                quantidadeDeDivisores++;
+            }
+            if (quantidadeDeDivisores >= 3) {
+                Console.WriteLine(false);
+            }
+        }
+         Console.WriteLine(quantidadeDeDivisores == 3);
+    }
+}
